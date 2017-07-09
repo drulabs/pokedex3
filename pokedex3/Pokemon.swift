1d7d9199bf5f61e246a5d66c42e25389f5788984
drulabs <kaushal.devil009@gmail.com>
2017-07-09 16:53:36 +0530

    Added sounds, csv reading etc. Made content more dynamic

---
+++ pokedex3/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  pokedex3
+//
+//  Created by Kaushal Dhruw on 09/07/17.
+//  Copyright © 2017 drulabs Inc. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    fileprivate var _name: String!
+    fileprivate var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
